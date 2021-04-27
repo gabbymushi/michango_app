8584f2694170f5387b5e90a9d673000997daac5c
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2021-04-27 13:45:00 +0300

    separete navigation drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
+import './drawer.dart';
 
 void main() {
   return runApp(MichangoApp());
 }
 
-class MichangoApp extends StatefulWidget {
-  @override
-  _MichangoAppState createState() => _MichangoAppState();
-}
-
-class _MichangoAppState extends State<MichangoApp> {
+class MichangoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,46 +15,13 @@ class _MichangoAppState extends State<MichangoApp> {
         accentColor: Colors.amber,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('HARUSI YA FRANK'),
-        ),
-        body: Column(),
-        drawer: Drawer(
-          // Add a ListView to the drawer. This ensures the user can scroll
-          // through the options in the drawer if there isn't enough vertical
-          // space to fit everything.
-          child: ListView(
-            // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Text('HARUSI YA FRANK'),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-              ListTile(
-                title: Text('Item 1'),
-                onTap: () {
-                  // Update the state of the app
-                  // ...
-                  // Then close the drawer
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text('Item 2'),
-                onTap: () {
-                  // Update the state of the app
-                  // ...
-                  // Then close the drawer
-                  Navigator.pop(context);
-                },
-              ),
-            ],
+          appBar: AppBar(
+            title: Text('HARUSI YA FRANK'),
           ),
-        ),
-      ),
+          body: Center(
+            child: Text('My Page'),
+          ),
+          drawer: MyDrawer()),
     );
   }
 }
