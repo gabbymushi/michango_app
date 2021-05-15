73e71d44a4fdbb84885719609d1e6af9fb06543e
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2021-05-15 13:43:31 +0300

    create color util and home page

---
+++ lib/pages/widgets/widget_home_category.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:michango/utils/hex_color.dart';
+
+class DashboardItems extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildListCategory(context),
+        ],
+      ),
+    );
+  }
+}
+
+_buildListCategory(BuildContext context) {
+  MediaQueryData mediaQueryData = MediaQuery.of(context);
+
+  return Container(
+    height: 150,
+    child: GridView.count(
+      crossAxisCount: 3,
+      children: [
+        makeDashboardItem(
+            mediaQueryData,
+            "Active\nUsers",
+            "#3FC379",
+            Icon(Icons.verified_user),
+            Border(
+              right: BorderSide(
+                color: Colors.black,
+                width: 1.0,
+              ),
+              bottom: BorderSide(
+                color: Colors.black,
+                width: 1.0,
+              ),
+            ),
+            () => {}),
+        makeDashboardItem(
+            mediaQueryData,
+            "Inactive Users",
+            "#FFCD42",
+            Icon(Icons.help),
+            Border(
+              right: BorderSide(
+                color: Colors.black,
+                width: 1.0,
+              ),
+              bottom: BorderSide(
+                color: Colors.black,
+                width: 1.0,
+              ),
+            ),
+            () => {}),
+        makeDashboardItem(
+            mediaQueryData,
+            "Deleted\nUsers",
+            "#D13839",
+            Icon(Icons.delete),
+            Border(
+              right: BorderSide(
+                color: Colors.black,
+                width: 1.0,
+              ),
+              bottom: BorderSide(
+                color: Colors.black,
+                width: 1.0,
+              ),
+            ),
+            () => {}),
+      ],
+    ),
+  );
+}
+
+Card makeDashboardItem(MediaQueryData query, String title, String color,
+    Icon icon, Border border, Function onTap) {
+  double width = query.size.width;
+  double fontSize = (width * 4) / 100;
+  double imageSize = (width * 18) / 100;
+
+  return Card(
+    elevation: 5,
+    color: HexColor(color),
+    margin: new EdgeInsets.all(5.0),
+    child: Container(
+      decoration: BoxDecoration(color: new HexColor(color)),
+      child: Center(
+        child: new InkWell(
+          onTap: onTap,
+          child: FractionalTranslation(
+            translation: Offset(0.0, 0.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              verticalDirection: VerticalDirection.down,
+              children: [
+                Center(
+                  child: ClipOval(
+                    child: Container(
+                      height: imageSize,
+                      width: imageSize,
+                      child: Center(
+                        child: icon,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                new Center(
+                  child: new Text(
+                    title.toUpperCase(),
+                    style: new TextStyle(
+                        fontSize: fontSize,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 5.0)
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
