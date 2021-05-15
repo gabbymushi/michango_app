165978eb51a960681643daacb6f9bc4baac78f6a
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2021-05-15 13:42:32 +0300

    create dashboard item maker

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:michango/pages/widgets/widget_home_category.dart';
 import './drawer.dart';
 
 void main() {
@@ -9,19 +10,40 @@ class MichangoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      //debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.light,
-        accentColor: Colors.amber,
+        accentColor: Colors.redAccent,
       ),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('HARUSI YA FRANK'),
-          ),
-          body: Center(
-            child: Text('My Page'),
-          ),
-          drawer: MyDrawer()),
+        appBar: AppBar(
+          title: Text('HARUSI YA FRANK.'),
+        ),
+        body: _uisetup(),
+        //drawer: MyDrawer(),
+      ),
     );
   }
+}
+
+Widget _uisetup() {
+  return new Container(
+    alignment: Alignment.center,
+    child: new Column(
+      children: <Widget>[
+        new Expanded(
+            child: Padding(
+          padding: EdgeInsets.all(0),
+          child: ListView(
+            shrinkWrap: true,
+            physics: BouncingScrollPhysics(),
+            children: [
+              DashboardItems(),
+              //Text('Widget 2'),
+            ],
+          ),
+        ))
+      ],
+    ),
+  );
 }
