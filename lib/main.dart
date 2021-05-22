89a6709131a3d524470524255e003f41449f43bf
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2021-05-23 01:45:11 +0300

    call add contributor dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:michango/pages/widgets/add_contributor.dart';
 import 'package:michango/pages/widgets/widget_home_category.dart';
-import 'pages/widgets/drawer.dart';
+//import 'pages/widgets/drawer.dart';
 
 void main() {
-  return runApp(MichangoApp());
-}
-
-class MichangoApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  return runApp(
+    MaterialApp(
       //debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.light,
         accentColor: Colors.redAccent,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('HARUSI YA FRANK.'),
-        ),
-        body: _uisetup(),
-        //drawer: MyDrawer(),
+      home: MichangoApp(),
+    ),
+  );
+}
+
+class MichangoApp extends StatelessWidget {
+  void _openAddEntryDialog(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (_) {
+            return AddEntryDialog();
+          },
+          fullscreenDialog: true),
+    );
+    //Navigator.of(context).push(AddEntryDialog());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HARUSI YA FRANK.'),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _openAddEntryDialog(context),
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      body: _uisetup(),
+      //drawer: MyDrawer(),
     );
   }
 }
 
+/* void _openAddEntryDialog(){
+  Navigator.of(context).push(MaterialPageRoute<null>(builder: (Build context){
+    return new AddEntryDialog();
+  },
+  fullScreenDialog:true,);
+} */
+
 Widget _uisetup() {
-  return new Container(
+  return Container(
     alignment: Alignment.center,
-    child: new Column(
+    child: Column(
       children: <Widget>[
-        new Expanded(
+        Expanded(
             child: Padding(
           padding: EdgeInsets.all(0),
           child: ListView(
