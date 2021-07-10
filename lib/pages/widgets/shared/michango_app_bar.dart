fb84d3c84875a6c77eb030db040ae72ab2563d12
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2021-07-10 11:33:00 +0300

    create shared directory

---
+++ lib/pages/widgets/shared/michango_app_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MichangoAppBar extends StatelessWidget implements PreferredSizeWidget{
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Center(
+        child: DropdownButton<String>(
+          value: 'HARUSI YA FRANK',
+          icon: Icon(
+            Icons.arrow_drop_down,
+            color: Colors.white,
+          ),
+          iconSize: 24,
+          elevation: 16,
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 19,
+              fontWeight: FontWeight.w200,
+              fontFamily: 'Encode Sans'),
+          /*    underline: Container(
+                height: 2,
+                color: Colors.white,
+              ), */
+          onChanged: (String newValue) {
+            /*   setState(() {
+          dropdownValue = newValue!;
+        }); */
+          },
+          items: <String>['HARUSI YA FRANK', 'SENDOFF YA ESHER']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
