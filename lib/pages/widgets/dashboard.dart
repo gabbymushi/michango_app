da31307a7d9fb3339ef9af493d5808ec8cb00547
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2021-07-10 14:20:10 +0300

    rearrange tiles

--- lib/pages/widgets/dashboard.dart
+++ lib/pages/widgets/dashboard.dart
@@ -1,71 +1,68 @@
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
+  final List<Map<String, dynamic>> categories = [
+    {
+      'color': Colors.green[200],
+      'title': 'Michango',
+      'icon': Icons.monetization_on
+    },
+    {'color': Colors.pink[200], 'title': 'Wauzaji', 'icon': Icons.shop},
+     {
+      'color': Colors.purple[200],
+      'title': 'Manage events',
+      'icon': Icons.event
+    },
+    {'color': Colors.blue[200], 'title': 'Settings', 'icon': Icons.settings},
+   
+
+    /*   {'color': Colors.pink[200], 'title': 'Walk', 'icon': Icons.directions_walk},
+    {'color': Colors.blue[200], 'title': 'Contact', 'icon': Icons.contact_mail},
+    {
+      'color': Colors.green[200],
+      'title': 'Bicycle',
+      'icon': Icons.directions_bike
+    },
+    {'color': Colors.pink[200], 'title': 'Boat', 'icon': Icons.directions_boat}, */
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(15.0),
-                child: MaterialButton(
-                  height: 100.0,
-                  minWidth: 130.0,
-                  color: Theme.of(context).primaryColor,
-                  textColor: Colors.white,
-                  child: Text("Michango"),
-                  onPressed: () => {},
-                  splashColor: Colors.redAccent,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(15.0),
-                child: MaterialButton(
-                  height: 100.0,
-                  minWidth: 130.0,
-                  color: Theme.of(context).primaryColor,
-                  textColor: Colors.white,
-                  child: Text("Ukumbi"),
-                  onPressed: () => {},
-                  splashColor: Colors.redAccent,
-                ),
+    return GridView.custom(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: (2 / 1),
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+        ),
+        childrenDelegate: SliverChildListDelegate(
+          categories
+              .map(
+                (data) => GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pushNamed('/michango');
+                    },
+                    child: Container(
+                      width: 200,
+                      height: 400,
+                      padding: const EdgeInsets.all(5),
+                      color: data['color'],
+                      child: Column(
+                        children: [
+                          Icon(
+                            data['icon'],
+                            size: 40,
+                          ),
+                          Text(data['title'],
+                              style:
+                                  TextStyle(fontSize: 22, color: Colors.white),
+                              textAlign: TextAlign.center)
+                        ],
+                      ),
+                    )),
               )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(15.0),
-                child: MaterialButton(
-                  height: 100.0,
-                  minWidth: 130.0,
-                  color: Theme.of(context).primaryColor,
-                  textColor: Colors.white,
-                  child: Text("MCs"),
-                  onPressed: () => {},
-                  splashColor: Colors.redAccent,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(15.0),
-                child: MaterialButton(
-                  height: 100.0,
-                  minWidth: 130.0,
-                  color: Theme.of(context).primaryColor,
-                  textColor: Colors.white,
-                  child: Text("Wapiga Picha"),
-                  onPressed: () => {},
-                  splashColor: Colors.redAccent,
-                ),
-              )
-            ],
-          )
-        ],
-      ),
-    );
+              .toList(),
+        ));
   }
 }
