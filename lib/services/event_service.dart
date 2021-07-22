a9dceefd77236685f4cb1e03a7fe0080137688ac
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2021-07-22 10:52:01 +0300

    create service module

---
+++ lib/services/event_service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:michango/models/user.dart';
+
+class UserService {
+  final Uri apiUrl = Uri.parse('http://localhost:3000/api/v1/users');
+
+  Future<User> createUser(User user) async {
+    Map data = {
+      'fullName': user.fullName,
+      'phoneNumber': user.phoneNumber,
+      'type': user.type
+    };
+
+    final Response response = await post(apiUrl,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(data));
+
+    if (response.statusCode == 200) {
+      return User.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to regiser user');
+    }
+  }
+}
