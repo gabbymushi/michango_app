6d7a172027ff8a23013bf77c482072c6d658f023
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2021-09-05 23:49:10 +0300

    added base url

--- lib/services/event_service.dart
+++ lib/services/event_service.dart
@@ -5,11 +5,13 @@ import 'package:michango/models/event.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class EventService {
-  final String baseUrl='http://172.20.10.2:3000/api/v1';
-  final Uri apiUrl = Uri.parse('http://172.20.10.2:3000/api/v1/events');
+  final String baseUrl = 'http://172.20.10.2:3000/api/v1';
+
   //final Uri apiUrl = Uri.parse('http://192.168.1.174:3000/api/v1/events');
 
   Future<Event> createInitialEvent(body) async {
+    final Uri apiUrl = Uri.parse('$baseUrl/events');
+
     final Response response = await post(apiUrl,
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
@@ -24,6 +26,8 @@ class EventService {
   }
 
   Future<Event> createEvent(Event event) async {
+    final Uri apiUrl = Uri.parse('$baseUrl/events');
+
     Map data = {'name': event.name, 'date': event.date};
 
     final Response response = await post(apiUrl,
@@ -41,15 +45,15 @@ class EventService {
 
   Future<List<Event>> getEvents() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    final Uri apiUrl = Uri.parse(
-        "http://172.20.10.2:3000/api/v1/users/${prefs.getString('userId')}/events");
+    final Uri apiUrl =
+        Uri.parse("$baseUrl/users/${prefs.getString('userId')}/events");
     Response response = await get(apiUrl, headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     });
 
     if (response.statusCode == 200) {
       var body = jsonDecode(response.body);
-     // print(response.body);
+      // print(response.body);
       return body.map<Event>((event) => Event.fromJson(event)).toList();
     } else {
       throw Exception('Failed to get events');
